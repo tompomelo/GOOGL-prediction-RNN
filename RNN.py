b370929d8e60c0835eca2259de5fb1546796f920
Tom Still <tom.still@live.co.uk>
2019-04-07 13:01:26 +0100

    Added Reshaping

--- RNN.py
+++ RNN.py
@@ -23,3 +23,7 @@ for i in range(60, 1258):
   X_train.append(training_set_scaled[i-60:i, 0])
   y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Reshape
+
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
